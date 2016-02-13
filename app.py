06552bdca3686362faa04faea1696b5a2c355a23
vickydasta <vickydasta@gmail.com>
2016-02-13 13:26:58 +0700

    version: beta

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from bs4 import BeautifulSoup as bs
 import aiml 
 
 bot = aiml.Kernel()
+app = Flask(__name__)
+
 try:
     print '[+] loading bot brain'
     bot.learn('brain/brain.aiml')
@@ -12,17 +13,14 @@ try:
 except Exception as e:
     raise Exception(e)
 
-app = Flask(__name__)
-
 @app.route('/')
 def homepage():
-    return render_template("index.html")
+    return render_template("app.html")
 
 @app.route('/apps', methods=['GET','POST'])
 def echo():
     answer = bot.respond(request.form['text'])
-    
-    return render_template("index.html",data=answer)
+    return render_template("app.html",data=answer)
 
 if __name__ == "__main__":
 	app.run(threaded=True, debug = True, host='0.0.0.0', port=8888, passthrough_errors=True)
