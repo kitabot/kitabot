90586e0d7e0a4f79502c86ac4ccb8df4433a09b5
vickydasta <vickydasta@gmail.com>
2016-02-14 10:52:38 +0700

    version: beta-1

--- app.py
+++ app.py
@@ -2,25 +2,28 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import aiml 
+import sys
 
+appPort = sys.argv[1:]
 bot = aiml.Kernel()
 app = Flask(__name__)
 
-try:
-    print '[+] loading bot brain'
-    bot.learn('brain/brain.aiml')
-    print '[+] brain loaded'
-except Exception as e:
-    raise Exception(e)
-
 @app.route('/')
 def homepage():
-    return render_template("app.html")
+    return render_template("index.html")
 
 @app.route('/apps', methods=['GET','POST'])
 def echo():
     answer = bot.respond(request.form['text'])
-    return render_template("app.html",data=answer)
+    return render_template("index.html", data=answer)
 
 if __name__ == "__main__":
-	app.run(threaded=True, debug = True, host='0.0.0.0', port=8888, passthrough_errors=True)
+	if appPort:
+		print '[+] loading bot brain'
+		bot.learn('brain/brain.aiml')
+		print '[+] brain loaded'
+		port = int(appPort[0])
+		app.run(threaded=True, debug = True, host='0.0.0.0', port=port, passthrough_errors=True)
+	else:
+		print '{} <port>'.format(sys.argv[0])
+		sys.exit(0)
