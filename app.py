b40a6b3ec437056a2e68953554f981127d500b64
vickydasta <vickydasta@gmail.com>
2016-02-16 17:58:13 +0700

    another commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import os
 import aiml 
-import sys
 
-appPort = sys.argv[1:]
+
 bot = aiml.Kernel()
 app = Flask(__name__)
 
@@ -15,22 +15,11 @@ def homepage():
 @app.route('/apps', methods=['GET','POST'])
 def echo():
     answer = bot.respond(request.form['text'])
-<<<<<<< HEAD
     return render_template("index.html", data=answer)
 
 if __name__ == "__main__":
-	if appPort:
-		print '[+] loading bot brain'
-		bot.learn('brain/brain.aiml')
-		print '[+] brain loaded'
-		port = int(appPort[0])
-		app.run(threaded=True, debug = True, host='0.0.0.0', port=port, passthrough_errors=True)
-	else:
-		print '{} <port>'.format(sys.argv[0])
-		sys.exit(0)
-=======
-    return render_template("index.html",data=answer)
-
-if __name__ == "__main__":
-	app.run(threaded=True, debug = True, host='0.0.0.0', port=8888, passthrough_errors=True)
->>>>>>> 7984a176d9d69a77112dcc9dfd9e8031b6c7e7c2
+	print '[+] loading bot brain'
+	bot.learn('brain/brain.aiml')
+	print '[+] brain loaded'
+	port = int(os.getenv('PORT', 33507))
+	app.run(threaded=True, debug = True, host='0.0.0.0', port=port, passthrough_errors=True)
